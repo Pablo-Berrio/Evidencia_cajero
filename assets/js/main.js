55aa4366d77242ff2009a3c9da751ca257eb6c84
Pablo Berrio <pabloberrio14@outlook.es>
2023-10-07 18:01:38 -0500

    se agregan ventanas modales

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
+const abrirModal = document.querySelector(".iniciar-sesion");
+const cerrarModal = document.getElementById("cerrar-modal")
+const modal = document.querySelector(".modal");
+
+abrirModal.addEventListener("click", () => {
+    modal.classList.add("modal--show");
+});
+
+cerrarModal.addEventListener("click", () => {
+    modal.classList.remove("modal--show");
+});
+
+const abrirModal2=document.querySelector(".registrar-cuenta")
+const cerrarModal2=document.getElementById("cerrar-modal2")
+const modal2=document.querySelector(".modal2")
+
+abrirModal2.addEventListener("click",()=>{
+    modal2.classList.add("modal--show2")
+})
+
+cerrarModal2.addEventListener("click",()=>{
+    modal2.classList.remove("modal--show2")
+})
+
+
 
 
 
